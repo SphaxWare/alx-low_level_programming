e78709d613a19cb103c491ec054767dfcc34362d
SphaxWare <itssphax123@gmail.com>
2023-07-14 18:40:21 -0700

    1-compiled no error pls

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -12,13 +12,13 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
 	if((n%10)>5){
-		printf("Last digit of %d is %d%10 and greater than 5",n);
+		printf("Last digit of %d is %d and greater than 5",n,n%10);
 	}
 	else if((n%10)==0){
-		printf("Last digit of %d is %d%10 and is 0",n);
+		printf("Last digit of %d is %d and is 0",n,n%10);
 	}
-	else if((n%10)<6){
-		printf("Last digit of %d is %d%10 and greater than 6 and not 0",n);
+	else if((n%10)<6 && (n%10)!=0){
+		printf("Last digit of %d is %d and greater than 6 and not 0",n,n%10);
 	}
 	return (0);
 }

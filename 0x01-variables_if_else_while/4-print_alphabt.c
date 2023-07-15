6cc4c2ffdf24b5a42c52dfb0b4dea01beaf32aab
SphaxWare <itssphax123@gmail.com>
2023-07-14 19:21:40 -0700

    go

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,23 +1,23 @@
 #include <stdio.h>
 
 /**
- * main - Prints the alphabet in lowercase and uppercase.
+ * main - Prints the alphabet in lowercase, excluding 'q' and 'e'.
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-        char ch;
+	char ch;
 
-        for (ch = 'a'; ch <= 'z'; ch++)
-        {
-                if(ch != 'q' && ch != 'e')
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		if (ch != 'q' && ch != 'e')
 		{
 			putchar(ch);
 		}
-        }
+	}
 
-        putchar('\n');
+	putchar('\n');
 
-        return (0);
+	return (0);
 }

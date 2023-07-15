6c778969bdc4645bbacdfd115d48a598cfa3b657
SphaxWare <itssphax123@gmail.com>
2023-07-14 19:09:52 -0700

    com

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,23 +1,27 @@
 #include <stdio.h>
+
 /**
- * main - Prints the alphabet.
+ * main - Prints the alphabet in lowercase and uppercase.
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	char ch;
+        char ch;
 
-	for (ch = 'a'; ch <= 'z'; ch++)
-	{
-		putchar(ch);
-	}
-
-	for (ch = 'A'; ch <= 'Z'; ch++)
+        for (ch = 'a'; ch <= 'z'; ch++)
         {
                 putchar(ch);
         }
 
-	putchar('\n');
-	return (0);
+        putchar('\n');
+
+        for (ch = 'A'; ch <= 'Z'; ch++)
+        {
+                putchar(ch);
+        }
+
+        putchar('\n');
+
+        return (0);
 }

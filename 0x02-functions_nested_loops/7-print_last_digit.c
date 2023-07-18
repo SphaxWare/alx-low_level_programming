1b00a338abef6bdfe850854fa1843ad4116e2473
SphaxWare <itssphax123@gmail.com>
2023-07-18 13:43:17 -0700

    last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _abs - does what its name says
+ *
+ * @c:ascii number for alphabet
+ *
+ * Description:this function print sign
+ *
+ *Return: 1 0 or -1
+ */
+int print_last_digit(int i)
+{
+	return(i%10);
+}

12180d382cd0ea2e9cab4e62b370109efeedae71
SphaxWare <itssphax123@gmail.com>
2023-07-18 09:21:39 -0700

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -3,10 +3,13 @@
 /**
  * print _putchar
  *
+ * main - Entry point of the program
+ *
+ * Return:Always 0 (Success)
  */
 int main(void)
 {
-	printf ("_putchar\n");
+	printf("_putchar\n");
 
-	return(0);
+	return (0);
 }

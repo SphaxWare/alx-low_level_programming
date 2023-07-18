51bbc2698d124f0490f78ea48cbbac0452549966
SphaxWare <itssphax123@gmail.com>
2023-07-18 09:40:02 -0700

    alphabet game

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - Entry point of the program
+ *
+ * description - print _putchar
+ *
+ * Return:Always 0 (Success)
+ */
+int main(void)
+{
+	void print_alphabet(void)
+	{
+		char ch;
+
+		while( ch = 'a'; ch => 'z'; ch++)
+		{
+			putchar(ch);
+		}
+		
+		putchar('\n')
+	}
+
+	return (0);
+}

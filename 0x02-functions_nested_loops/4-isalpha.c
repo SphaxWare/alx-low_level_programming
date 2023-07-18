30189f12c1e14b8ef6c83f839c04834a8482eb35
SphaxWare <itssphax123@gmail.com>
2023-07-18 12:24:47 -0700

    isalpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * _isalpha - does what its name says
+ *
+ * @c:ascii number for alphabet
+ *
+ * Description:this function print all the alphabets
+ *
+ *Return: 1 if lowercase and 0 if not
+ */
+int _isalpha(int c)
+{
+	if (c >= 97 && c <= 122 || c >= 65 && c <= 90)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}

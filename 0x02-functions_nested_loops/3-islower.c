9c2f49c4294109a467f6c5994f8bb1dbc08e74dc
SphaxWare <itssphax123@gmail.com>
2023-07-18 12:17:07 -0700

    islower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,11 +2,15 @@
 /**
  * _islower - does what its name says
  *
+ * @c:ascii number for alphabet
+ *
  * Description:this function print all the alphabets
+ *
+ *Return: 1 if lowercase and 0 if not
  */
 int _islower(int c)
 {
-	if(c >= 97 && c <= 122)
+	if (c >= 97 && c <= 122)
 	{
 		return (1);
 	}

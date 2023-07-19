2c4e05f17097c7bbd6feacf8b722d3b96aa3f23f
SphaxWare <itssphax123@gmail.com>
2023-07-18 19:11:26 -0700

    13-

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,21 +1,24 @@
-#include<stdio.h>
+#include <stdio.h>
 
 /**
- * main - prints the n times table, starting with 0
- * return:0
+ * main - Prints the sum of all numbers divisible by 3 or 5 below 1024.
+ *
+ * Return: Always 0.
  */
 int main(void)
 {
-	int i, sum = 0;
+    int i = 0, sum = 0;
 
-	while (i < 1024)
-	{
-		if (i % 3 == 0 || i % 5 == 0)
-		{
-			sum += i;
-		}
-	i++;
-	}
-	printf("%d\n", sum);
-	return (0);	
+    while (i < 1024)
+    {
+        if (i % 3 == 0 || i % 5 == 0)
+        {
+            sum += i;
+        }
+        i++;
+    }
+
+    printf("%d\n", sum);
+
+    return 0;
 }

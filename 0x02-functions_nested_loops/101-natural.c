fc81943bd067d89733179f9706d245e8ab14653a
SphaxWare <itssphax123@gmail.com>
2023-07-18 19:14:03 -0700

    13-

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -7,18 +7,18 @@
  */
 int main(void)
 {
-    int i = 0, sum = 0;
+	int i = 0, sum = 0;
 
-    while (i < 1024)
-    {
-        if (i % 3 == 0 || i % 5 == 0)
-        {
-            sum += i;
-        }
-        i++;
-    }
+	while (i < 1024)
+	{
+		if (i % 3 == 0 || i % 5 == 0)
+		{
+			sum += i;
+		}
+	i++;
+	}
 
-    printf("%d\n", sum);
+	printf("%d\n", sum);
 
-    return 0;
+	return 0;
 }

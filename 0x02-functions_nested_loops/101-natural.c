8a4e3b3f4f2add1ae1d15870dd75822831286c8a
SphaxWare <itssphax123@gmail.com>
2023-07-18 19:00:24 -0700

    13-

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+/**
+ * main - prints the n times table, starting with 0
+ * return:0
+ */
+int main()
+{
+	int i, sum = 0;
+
+	while (i < 1024)
+	{
+		if (i % 3 == 0 || i % 5 == 0)
+		{
+			sum += i;
+		}
+	i++;
+	}
+	printf('%d',sum);
+	return (0);	
+}

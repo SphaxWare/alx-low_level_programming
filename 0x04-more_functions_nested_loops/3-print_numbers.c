1df5b3e59b42f5fc97147bbbdbd8b3724759db30
SphaxWare <itssphax123@gmail.com>
2023-07-20 14:46:12 -0700

    run it up

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,13 +1,14 @@
 #include "main.h"
 
 /**
- * main - check the code
+ * print_numbers - check the code
  *
  * Return: Always 0.
  */
 void print_numbers(void)
 {
 	int i;
+
 	for (i = 48; i <= 57; i++)
 	{
 		_putchar (i);

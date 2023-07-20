93143ec34b49acdb577f211b1ab84b7930a595b3
SphaxWare <itssphax123@gmail.com>
2023-07-20 12:43:08 -0700

    isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _isupper - check if UPPER.
+ *
+ * @c:variable 
+ *
+ * Return: Always 0.
+ */
+
+int _isupper(int c)
+{
+	if (c >= 90 && c >= 65)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}

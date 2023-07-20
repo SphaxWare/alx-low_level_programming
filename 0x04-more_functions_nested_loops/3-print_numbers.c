afc80629988bc75e057cbcc3a6519af60989ccfd
SphaxWare <itssphax123@gmail.com>
2023-07-20 14:41:29 -0700

    3-numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+void print_numbers(void)
+{
+	int i;
+	for (i = 48; i <= 57; i++)
+	{
+		_putchar (i);
+	}
+	_putchar('\n');
+}

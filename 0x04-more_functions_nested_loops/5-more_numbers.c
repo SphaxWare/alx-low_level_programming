cbdc20275adec77fddccf856d97ad09e879f681d
SphaxWare <itssphax123@gmail.com>
2023-07-20 16:23:11 -0700

    More numbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * more_numbers - check the code
+ *
+ * Return: Always 0.
+ */
+void more_numbers(void)
+{
+	int i, j, k;
+
+	for (i = 0; i <= 10; i++)
+	{
+		for (j = 0; j <= 14; j++)
+		{
+			if (j > 9)
+			{
+				_putchar((j / 10) + 48);
+			}
+			k = j % 10;
+			_putchar(k + 48);
+		}
+		_putchar('\n');
+	}
+}

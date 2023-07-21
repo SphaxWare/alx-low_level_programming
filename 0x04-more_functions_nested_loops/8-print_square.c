f3ca2b43f774b80f75f223f58ad1e56785ef7f9e
SphaxWare <itssphax123@gmail.com>
2023-07-20 17:44:24 -0700

    square

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -2,12 +2,16 @@
 
 /**
  * print_square - check the code
- * @n:int
+ * @size:int
  */
 void print_square(int size)
 {
 	int i, j;
 
+	if ( size <= 0)
+	{
+		_putchar('\n');
+	}
 	for (i = 0; i < size; i++)
 	{
 		for (j = 0; j < size; j++)

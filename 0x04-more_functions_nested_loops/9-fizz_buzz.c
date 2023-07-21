f7a65906801e7f70b476ba70eaa439b542e30677
SphaxWare <itssphax123@gmail.com>
2023-07-20 18:25:24 -0700

    fizzbuzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ */
+int main(void)
+{
+	int i;
+
+	for(i = 0; i <= 100; i++)
+	{
+		if (((i % 3) == 0) && ((i % 5) == 0))
+		{
+			printf("FizzBuzz ");
+		}
+		else if ((i % 3) == 0)
+		{
+			printf("Fizz ");
+		}
+		else if ((i % 5) == 0)
+		{
+			printf("Buzz ");
+		}
+		else
+		{
+			printf("%d ",i);
+		}
+	}
+	return (0);
+}

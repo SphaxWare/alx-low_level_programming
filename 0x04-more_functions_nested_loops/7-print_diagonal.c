6413c12fb683a556cef3bc8cf2f7d15ea56c9ef2
SphaxWare <itssphax123@gmail.com>
2023-07-20 17:01:17 -0700

    diagonal

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -6,14 +6,22 @@
  */
 void print_diagonal(int n)
 {
-	int i;
+	int i, j;
 
 	for (i = 0; i <= n; i++)
 	{
-		if (i != 0)
+		if (i == 0)
 		{
-			_putchar('\\');
+			_putchar('\n');
 		}
-		_putchar('\n');
+		else
+		{
+			for (j = 0; j < i; j++)
+			{
+				_putchar(' ');
+			}
+			_putchar('\\');
+			_putchar('\n');
+		}
 	}
 }

0b6a393dbe480b700c39b53b0c0985b75038ecd3
SphaxWare <itssphax123@gmail.com>
2023-08-01 17:08:19 -0700

    strstr

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -12,7 +12,9 @@ char *_strstr(char *haystack, char *needle)
 	for (; *haystack != '\0'; haystack++)
 	{
 		char *i = haystack;
+
 		char *p = needle;
+
 		while (*i == *p && *p != '\0')
 		{
 			i++;

f8e4abdee8e1500709a330611a1549109211d231
SphaxWare <itssphax123@gmail.com>
2023-08-01 17:26:15 -0700

    diagsum

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_diagsums - Entry point
+ * @a: int
+ * @size:size
+ * Return: Always 0 (Success)
+ */
+void print_diagsums(int *a, int size)
+{
+	int i;
+
+	int sum1 = 0;
+
+	int sum2 = 0;
+
+	for (i = 0; i <= size; i++)
+	{
+		sum1 = sum1 + a[i * size + i];
+		sum2 = sum2 + a[i * size + i];
+	}
+}

efd73438dc23f25f5944e3c7b59418b5ff6fa4ea
SphaxWare <itssphax123@gmail.com>
2023-08-01 17:34:48 -0700

    diagsum

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -19,5 +19,5 @@ void print_diagsums(int *a, int size)
 		sum1 = sum1 + a[i * size + i];
 		sum2 = sum2 + a[i * size + (size - 1 - i)];
 	}
-	printf("%d, %d\n",sum1,sum2);
+	printf("%d, %d\n", sum1, sum2);
 }

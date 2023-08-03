be6c1a300bd89eaa2de11b27187ae082539255d6
SphaxWare <itssphax123@gmail.com>
2023-08-02 18:27:57 -0700

    puts res

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -6,9 +6,13 @@
  */
 void _puts_recursion(char *s)
 {
-	for (; *s != '\0'; s++)
+	if (*s != '\0')
 	{
 		_putchar(*s);
+		_puts_recursion(s+1);
 	}
-	_putchar('\n');
+	else
+	{
+		_putchar('\n');
+	}
 }

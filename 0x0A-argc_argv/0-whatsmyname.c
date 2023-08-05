0b92ed82613f0b3244df6b979edcbdcf7536dd8e
SphaxWare <itssphax123@gmail.com>
2023-08-05 10:45:50 -0700

    mynameis

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
-/*
- * main-print prog name
- * @argc: lenght
- * @argv: array
+/**
+ * main - prints the name of the program
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: Always 0 (Success)
  */
-int main(int __attribute__((unused)) argc, char* argv[])
+
+int main(int __attribute__((unused)) argc, char *argv[])
 {
-	printf("%s\n",argv[0]);
+	printf("%s\n", argv[0]);
 	return (0);
 }

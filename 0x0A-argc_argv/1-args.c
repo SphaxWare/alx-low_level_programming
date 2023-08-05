dd651b3e3185beb34070bcb18c5fe2259b4e14a7
SphaxWare <itssphax123@gmail.com>
2023-08-05 10:48:15 -0700

    args

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - prints the name of the program
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(int argc, char __attribute__((unused)) *argv[])
+{
+	printf("%d\n", argc);
+	return (0);
+}

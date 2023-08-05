e658acf749fa083197f9ebe6beea311f3f3ba148
SphaxWare <itssphax123@gmail.com>
2023-08-05 10:30:06 -0700

    myname

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/*
+ * main-print prog name
+ * @argc: lenght
+ * @argv: array
+ */
+int main(int __attribute__((unused)) argc, char* argv[])
+{
+	printf("%s \n",argv[0]);
+	return (0);
+}

b5ddbe4180e67659fe1a10d9d04c4ee50c0aeee7
SphaxWare <itssphax123@gmail.com>
2023-08-07 13:34:23 -0700

    0-malloc

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+/**
+ * create_array - create an array
+ * @size:size
+ * @c:char
+ *
+ * Return:Null or pointer
+ */
+char *create_array(unsigned int size, char c)
+{
+	if (size == 0)
+	{
+		return (NULL);
+	}
+	else
+	{
+		char *array[size];
+		array = malloc(sizeof(char) * size);
+		array[0] = c;
+		return (array);
+	}
+}
---
+++ 0x0B-malloc_free/main.h
@@ -0,0 +1,4 @@
+#ifndef main.h
+#define main.h
+char *create_array(unsigned int size, char c);
+#endif

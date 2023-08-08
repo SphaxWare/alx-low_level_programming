ae2ffe079dd92ba5a780468f383e0600185f68c8
SphaxWare <itssphax123@gmail.com>
2023-08-08 06:50:59 -0700

    1-mallooc

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+/**
+ * create_array - create an array
+ *
+ * @str:char
+ *
+ * Return:Null or pointer
+ */
+char *_strdup(char *str)
+{
+	char *pointer;
+	int i, r;
+
+	if (str == NULL)
+		return (NULL);
+	i = 0;
+	while (str[i] != '\0')
+		i++;
+
+	pointer = malloc(sizeof(char) * (i + 1));
+
+	if (pointer == NULL)
+		return (NULL);
+
+	for (r = 0; str[r]; r++)
+	{
+		pointer[r] = str[r];
+	}
+
+	return (pointer);
+}

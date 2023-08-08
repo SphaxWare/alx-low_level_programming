9187b1ce4640e2af7601b9e17801d8f1d11b4ac9
SphaxWare <itssphax123@gmail.com>
2023-08-08 09:15:40 -0700

    2darrayalloc

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <string.h>
+/**
+ * alloc_grid - create an array
+ *
+ * @width:char
+ * @height:char
+ *
+ * Return:Null or pointer
+ */
+int **alloc_grid(int width, int height)
+{
+	int **grid;
+	int w, h;
+	if (width <= 0 || height <= 0)
+		return (NULL);
+	grid = malloc(sizeof(int *) * height);
+	if (grid == NULL)
+		return (NULL);
+	for (h = 0; h < height; h++)
+	{
+		grid[h] = malloc(sizeof(int) * width);
+		for (w = 0; w < width; w++)
+		{
+			grid[h][w] = 0;
+		}
+	}
+	return (grid);
+}

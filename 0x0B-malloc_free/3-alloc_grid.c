bca10d5d6be99a119f4872fc111256140a7f8f9e
SphaxWare <itssphax123@gmail.com>
2023-08-08 09:17:38 -0700

    2darrayalloc

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -12,6 +12,7 @@ int **alloc_grid(int width, int height)
 {
 	int **grid;
 	int w, h;
+
 	if (width <= 0 || height <= 0)
 		return (NULL);
 	grid = malloc(sizeof(int *) * height);
@@ -20,6 +21,8 @@ int **alloc_grid(int width, int height)
 	for (h = 0; h < height; h++)
 	{
 		grid[h] = malloc(sizeof(int) * width);
+		if (grid[h] == NULL)
+			return (NULL);
 		for (w = 0; w < width; w++)
 		{
 			grid[h][w] = 0;

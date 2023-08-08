01500a1e99e01c1dcd8ff8d335573de5670d1529
SphaxWare <itssphax123@gmail.com>
2023-08-08 09:46:27 -0700

    2d array alloc

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -23,7 +23,7 @@ int **alloc_grid(int width, int height)
 		grid[h] = malloc(sizeof(int) * width);
 		if (grid[h] == NULL)
 		{
-			for(; h >= 0;h--)
+			for (; h >= 0; h--)
 				free(grid[h]);
 			free(grid);
 			return (NULL);

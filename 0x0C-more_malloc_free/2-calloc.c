bfb1732f949ca2e64ea930d576f678635fc42c86
SphaxWare <itssphax123@gmail.com>
2023-08-10 13:44:36 -0700

    calcc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,46 +1,46 @@
-#include <stdlib.h>
-#include "main.h"
+#include<stdlib.h>
+#include"main.h"
 
 /**
- * _memset - fills memory with a constant byte
- * @s: memory area to be filled
- * @b: char to copy
- * @n: number of times to copy b
+ *_memset - fills memory with a constant byte
+ *@s: memory area to be filled
+ *@b: char to copy
+ *@n: number of times to copy b
  *
- * Return: pointer to the memory area s
+ *Return: pointer to the memory area s
  */
 char *_memset(char *s, char b, unsigned int n)
 {
-    unsigned int i;
+	unsigned int i;
 
-    for (i = 0; i < n; i++)
-    {
-        s[i] = b;
-    }
+	for (i = 0; i < n; i++)
+	{
+		s[i] = b;
+	}
 
-    return (s);
+	return (s);
 }
 
 /**
- * _calloc - allocates memory for an array
- * @nmemb: number of elements in the array
- * @size: size of each element
+ *_calloc - allocates memory for an array
+ *@nmemb: number of elements in the array
+ *@size: size of each element
  *
- * Return: pointer to allocated memory
+ *Return: pointer to allocated memory
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-    char *ptr;
+	char *ptr;
 
-    if (nmemb == 0 || size == 0)
-        return (NULL);
+	if (nmemb == 0 || size == 0)
+		return (NULL);
 
-    ptr = malloc(size * nmemb);
+	ptr = malloc(size * nmemb);
 
-    if (ptr == NULL)
-        return (NULL);
+	if (ptr == NULL)
+		return (NULL);
 
-    _memset(ptr, 0, nmemb * size);
+	_memset(ptr, 0, nmemb * size);
 
-    return (ptr);
+	return (ptr);
 }

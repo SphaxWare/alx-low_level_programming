62135eb51dd043641a68dda97f88ffe928de6a04
SphaxWare <itssphax123@gmail.com>
2023-08-14 13:02:23 -0700

    struct dog

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * init_dog - initializes a variable of type struct dog
+ * @d: pointer to struct dog to initialize
+ * @name: name to initialize
+ * @age: age to initialize
+ * @owner: owner to initialize
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	struct dog dog1;
+
+	dog1.name = *name;
+	dog1.age = age;
+	dog1 = *owner;
+	return (0);
+}

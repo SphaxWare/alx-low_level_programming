8d542b476be7e5198b13efaf3483ec2a30083582
SphaxWare <itssphax123@gmail.com>
2023-08-14 14:37:52 -0700

    fixed some problemes

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,7 +2,7 @@
 #define DOG_H
 
 /**
- * struct dog - Short description
+ * struct dog - a dog's basic info
  * @name: First member
  * @age: Second member
  * @owner: Third member
@@ -11,12 +11,21 @@
  */
 struct dog
 {
-	char * name;
+	char *name;
 	float age;
-	char * owner;
+	char *owner;
 };
+
+/**
+ * dog_t - typedef for struct dog
+ */
+typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+char *_strcpy(char *dest, char *src);
+int _strlen(char *s);
 
 #endif

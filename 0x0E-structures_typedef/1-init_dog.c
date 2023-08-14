03a5fadd721ecc609f05a23f84d2bc136bc8bc8a
SphaxWare <itssphax123@gmail.com>
2023-08-14 13:08:32 -0700

    1-dog

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -10,8 +10,11 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
+	if (d == NULL)
+		return (0);
+
 	d.name = *name;
 	d.age = age;
-	d = *owner;
+	d.owner = *owner;
 	return (0);
 }

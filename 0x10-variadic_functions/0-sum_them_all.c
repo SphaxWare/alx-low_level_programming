273e40eb4aca420ea4c35502944c7225b16890cf
SphaxWare <itssphax123@gmail.com>
2023-08-17 08:24:11 -0700

    sum them all

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -12,12 +12,13 @@
 int sum_them_all(const unsigned int n, ...)
 {
 	va_list ap;
-	int i, sum = 0;
+	unsigned int i, sum;
 
 	if (n == 0)
-		retun (0);
+		return(0);
 	va_start (ap, n);
-	for (; i < n; i++)
+	sum = 0;
+	for (i = 0; i < n; i++)
 		sum += va_arg (ap, int);
 	va_end (ap);
 	return (sum);

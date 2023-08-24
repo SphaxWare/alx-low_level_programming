cc1435ea0ffb282f9947a4bdab7ba3450da91e5c
SphaxWare <itssphax123@gmail.com>
2023-08-24 15:56:18 -0700

    linked lists solving task 0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -13,15 +13,12 @@ size_t print_list(const list_t *h)
 	int count = 0;
 	while (h != NULL)
 	{
-		while (h)
-		{
-			if (h->str == NULL)
-				printf("[0] (nil)\n");
-			else
-				printf("[%u] %s\n", h->len, h->str);
-			h = h->next;
-			count++;
-		}
-
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%u] %s\n", h->len, h->str);
+		h = h->next;
+		count++;
 	}
+	return (count);
 }

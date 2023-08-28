4653ba3453bb73b68389b89f3e64879565b30de5
SphaxWare <itssphax123@gmail.com>
2023-08-28 11:35:44 -0700

    addnode

---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * add_nodeint - check the code
+ *
+ * Return: adress of new element or null.
+ */
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *new;
+
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
+		return (NULL);
+	new->n = n;
+	new->next = *head;
+	*head = new;
+}

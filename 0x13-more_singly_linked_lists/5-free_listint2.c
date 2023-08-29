344b117dc34ea6bd893d4f32d8612f3c87bd05bb
SphaxWare <itssphax123@gmail.com>
2023-08-29 10:50:37 -0700

    solving task 5

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * free_listint - check the code
+ * @head:pointer to the pointer to first elem
+ * Return: adress of new element or null.
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *current;
+
+	while (*head != NULL)
+	{
+		current = *head;
+		*head = *head->next;
+		free(current);
+	}
+	*head = NULL
+}

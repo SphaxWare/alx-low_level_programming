289e15c7fad2f0b2e7810def68beca814ae24297
SphaxWare <itssphax123@gmail.com>
2023-08-29 12:37:24 -0700

    solving task 9 some syntaxe error

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -26,7 +26,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	{
 		if (i == idx - 1)
 		{
-			new->next = *head->next;
+			new->next = (*head)->next;
 			new->n = n;
 			(*head)->next = new;
 			return (new);

2c5757afafe3e170ced87443f164eca591282e59
SphaxWare <itssphax123@gmail.com>
2023-08-29 09:11:10 -0700

    solving task 3 add node end

---
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+/**
+ * add_nodeint_end - check the code
+ * @head:pointer to the pointer to first elem
+ * @n:value to add to the new node
+ * Return: adress of new element or null.
+ */
+listint_t *add_nodeint_end(listint_t **head, const int n)
+{
+	listint_t *new;
+	listint_t *temp = *head;
+
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
+		return (NULL);
+	if (*head == NULL)
+	{
+		*head = new;
+		return (new);
+	}
+	new->n = n;
+	new->next = NULL;
+	while (temp->next != NULL)
+		temp = temp->next;
+	temp->next = new;
+	return (new);
+}

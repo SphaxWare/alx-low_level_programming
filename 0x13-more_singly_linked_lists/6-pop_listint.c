43af3fc9dd4593bdc716b7c56a708a1f4e7a1e96
SphaxWare <itssphax123@gmail.com>
2023-08-29 11:57:05 -0700

    solving task 6

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * pop_listint - check the code
+ * @head: pointer to the first element in the linked list
+ * Return: n or 0
+ */
+int pop_listint(listint_t **head)
+{
+	listint_t *temp;
+	int n;
+
+	if (head == NULL)
+		return (0);
+
+	n = (*head)->n;
+	temp = (*head)->next;
+	free(*head);
+	*head = temp;
+	return (n);
+}

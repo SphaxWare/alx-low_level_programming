c8063d3ed2a97f58f1289c8a0bb67dbb0b51a6c9
SphaxWare <itssphax123@gmail.com>
2023-08-29 09:53:18 -0700

    solving task 4

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,15 @@
+#include "lists.h"
+/**
+ * free_listint - check the code
+ * @head:pointer to the pointer to first elem
+ * @n:value to add to the new node
+ * Return: adress of new element or null.
+ */
+void free_listint(listint_t *head)
+{
+	while (head != NULL)
+	{
+		free(head);
+		head = head->next;
+	}
+}

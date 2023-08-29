7f8d814c380e5fdea722e39fe91c15f62d748fa2
SphaxWare <itssphax123@gmail.com>
2023-08-29 09:12:56 -0700

    solving checker problemes for task 3

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -13,13 +13,13 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 	new = malloc(sizeof(listint_t));
 	if (new == NULL)
 		return (NULL);
+	new->n = n;
+	new->next = NULL;
 	if (*head == NULL)
 	{
 		*head = new;
 		return (new);
 	}
-	new->n = n;
-	new->next = NULL;
 	while (temp->next != NULL)
 		temp = temp->next;
 	temp->next = new;

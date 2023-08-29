4e287357ac5b27e556848bfa76f86e4d87001f10
SphaxWare <itssphax123@gmail.com>
2023-08-29 12:13:13 -0700

    solving task 7 some mistakes

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -16,7 +16,7 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	while (temp != NULL && count < index)
 	{
 		temp = temp->next;
-		i++;
+		count++;
 	}
 	if (temp == NULL)
 		return (NULL);

d5a012878c665aab0aa5ee1214e5b591d1962192
SphaxWare <itssphax123@gmail.com>
2023-08-31 16:57:05 -0700

    solving task 3

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,12 @@
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	if (index > 63)
+		return (-1);
+	*n = ((1UL << index) | *n);
+	return (1);
+}

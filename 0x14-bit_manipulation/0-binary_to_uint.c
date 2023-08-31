6b49d246d491246a51c878b11632f4ba7f8765ce
SphaxWare <itssphax123@gmail.com>
2023-08-31 12:48:41 -0700

    solving problemes in task 0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -10,12 +10,12 @@ unsigned int binary_to_uint(const char *b)
 	int loop;
 	unsigned int converted = 0;
 
-	if (b != '/0')
+	if (!b)
 		return (0);
 
 	for (loop = 0; b[loop]; loop++)
 	{
-		if (b[loop] != '0' || b[loop] != '1')
+		if (b[loop] < '0' || b[loop] > '1')
 			return (0);
 		converted = 2 * converted + (b[loop] - 48);
 	}

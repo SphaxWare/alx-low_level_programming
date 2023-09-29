ff78e3ebe718ef03229846834a6704feea6182eb
SphaxWare <itssphax123@gmail.com>
2023-09-28 19:08:50 -0700

    solving advance

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * get_endianness - checks if a machine is little or big endian
+ * Return: 0 for big, 1 for little
+ */
+int get_endianness(void)
+{
+	unsigned int num = 1;
+	char *byte = (char *)&num;
+
+	return ((int)(*byte));
+}

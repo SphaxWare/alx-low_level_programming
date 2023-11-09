cfbd3e1e6fde967941addd71ee2fdfe9b2e1334d
SphaxWare <itssphax123@gmail.com>
2023-11-09 09:31:01 -0800

    fix 1

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -13,7 +13,10 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	if (new == NULL)
 		return (NULL);
 	new->next = *head;
-	(*head)->prev = new;
 	new->n = n;
-	return (new);	
+	new->prev = NULL;
+	if (*head != NULL)
+		(*head)->prev = new;
+	*head = new;
+	return (new);
 }

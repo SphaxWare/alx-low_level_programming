14409a40c5e6d094efcb6844b6cade0ec53e4135
SphaxWare <itssphax123@gmail.com>
2023-11-09 07:57:44 -0800

    print dlist

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * print_dlistint - check the code
+ * @h: head
+ * Return: int.
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	int i = 0;
+
+	while(h)
+	{
+		printf("%d\n", h->n)
+		i++;
+		h = h->next;
+	}
+	return (i);
+}

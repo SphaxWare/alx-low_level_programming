9663be7f8b6ef3e57e45ad89b97a37776bbdce82
SphaxWare <itssphax123@gmail.com>
2023-11-09 16:29:41 -0800

    delete

---
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -0,0 +1,38 @@
+#include "lists.h"
+/**
+ * delete_dnodeint_at_index - Delete node at nth
+ * @head: head
+ * @index: pos
+ * Return: 1 
+ */
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+	unsigned int i = 0;
+	dlistint_t *tmp = *head;
+
+	if (*head == NULL)
+		return (-1);
+	if (index == 0)
+	{
+		*head = tmp->next;
+		if (tmp->next != NULL)
+			tmp->next->prev = NULL;
+		free(tmp);
+		return (1);
+	}
+	while (tmp)
+	{
+		if (index == i)
+		{
+			if (tmp->next != NULL)
+				tmp->next->prev = tmp->prev;
+			if (tmp->prev != NULL)
+				tmp->prev->next = tmp->next;
+			free(tmp);
+			return (1);
+		}
+		i++;
+		tmp = tmp->next;
+	}
+	return (-1);
+}

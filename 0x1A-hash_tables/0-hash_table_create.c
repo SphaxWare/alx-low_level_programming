bb316431228614c48b4803cabf2094dd9422ae5d
SphaxWare <itssphax123@gmail.com>
2023-11-30 15:34:52 -0800

    fix 1

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -6,16 +6,16 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	struct hash_table_s *table;
+	hash_table_t *table;
 	unsigned long int i;
 
 	if (size == 0)
 		return (NULL);
-	table = malloc(sizeof(struct hash_table_s));
+	table = malloc(sizeof(hash_table_t));
 	if (table == NULL)
 		return (NULL);
 	table->size = size;
-	table->array = calloc(table->size, sizeof(struct hash_node_s));
+	table->array = calloc(table->size, sizeof(hash_node_t));
 	if (table->array == NULL)
 		return (NULL);
 	for (i = 0; i < table->size; i++)

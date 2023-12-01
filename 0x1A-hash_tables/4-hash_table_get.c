daa4c6094eb354fec449ea11c81bf44b688ac38b
SphaxWare <itssphax123@gmail.com>
2023-12-01 08:53:28 -0800

    fix

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -10,7 +10,8 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	unsigned long int index;
 	hash_node_t *find;
 
-	if (key == NULL || ht == NULL || strcmp(key, "") == 0)
+	if (key == NULL || ht == NULL || strcmp(key, "") == 0 ||
+	    ht->size == 0)
 		return (NULL);
 	index = key_index((const unsigned char *)key, ht->size);
 	if (ht->array[index] == NULL)

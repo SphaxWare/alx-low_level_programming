4cc5a5181a351f2c7cfc20e291bd2b4df049a236
Sphax <itssphax123@gmail.com>
2024-03-13 14:33:41 -0700

    jump ago

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -2,9 +2,14 @@
 #define SEARCH_ALGOS_H
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 void printer(int *array, int s, int e);
+int jump_search(int *array, size_t size, int value);
+int interpolation_search(int *array, size_t size, int value);
+int exponential_search(int *array, size_t size, int value);
+int advanced_binary(int *array, size_t size, int value);
 
 #endif
